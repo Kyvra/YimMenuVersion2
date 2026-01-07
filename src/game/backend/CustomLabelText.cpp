2386ceaaee1990d2bcd34349c7bf15b4da10832f
𝐶𝑆𝑌𝑂𝑁 <134310111+CSY0N@users.noreply.github.com>
2026-01-07 16:54:53 +0100

    Update CustomLabelText.cpp

--- src/game/backend/CustomLabelText.cpp
+++ src/game/backend/CustomLabelText.cpp
@@ -4,16 +4,16 @@ namespace YimMenu
 {
 	void CustomLabelText::InitImpl()
 	{
-		AddLabelImpl(0x1B2EA75B, "Grand Theft Auto V Story"); // Title Story Mode
-		AddLabelImpl(0xABB00DEB, "Grand Theft Auto V");       // Title Pause Menu
-		AddLabelImpl(Joaat("PM_ENTER_MP"), "Play Grand Theft Auto Online with YimMenu");
-		AddLabelImpl(Joaat("PM_EXIT_GAME"), "Quit Grand Theft Auto V with YimMenu");
+		AddLabelImpl(0x1B2EA75B, "YimMenu Story"); // Title Story Mode
+		AddLabelImpl(0xABB00DEB, "Csyon's YimMenu"); // Title Pause Menu
+		AddLabelImpl(Joaat("PM_ENTER_MP"), "Play GTA Online with YimMenu");
+		AddLabelImpl(Joaat("PM_EXIT_GAME"), "Quit GTA V with YimMenu");
 		AddLabelImpl(Joaat("PM_GO"), "Play with YimMenu");
 		AddLabelImpl(Joaat("HUD_MPREENTER"), "Joining a new session with YimMenu");
 		AddLabelImpl(Joaat("PM_FRIEND_FM"), "Closed Friend Session with YimMenu");
 		AddLabelImpl(Joaat("PM_INF_EXIT"), "Quit to desktop with YimMenu");
 		AddLabelImpl(Joaat("PM_CREWS"), "Join a Crew Only Session with YimMenu");
-		AddLabelImpl(Joaat("PM_INF_PGOT"), "Join Online with YimMenu");
+		AddLabelImpl(Joaat("PM_INF_PGOT"), "Online with YimMenu");
 		AddLabelImpl(Joaat("PM_INVO_FM"), "Join an Invite Only Session with YimMenu");
 		AddLabelImpl(Joaat("LOADING_SPLAYER_L"), "Loading Story Mode with YimMenu");
 		AddLabelImpl(Joaat("PM_NCREW_FM"), "Join a Crew Session with YimMenu");
@@ -21,7 +21,7 @@ namespace YimMenu
 		AddLabelImpl(Joaat("PM_SOLO_FM"), "Join a Solo Session with YimMenu");
 		AddLabelImpl(Joaat("PM_FRESES"), "Join a Friends Only Session with YimMenu");
 		AddLabelImpl(Joaat("PM_CHOOSE_CHAR"), "Choose your personnage with YimMenu");
-		AddLabelImpl(Joaat("PM_INF_PGOT"), "Join Online with YimMenu");
+		AddLabelImpl(Joaat("PM_INF_PGOT"), "Online with YimMenu");
 	}
 
 	bool CustomLabelText::AddLabelImpl(joaat_t hash, const std::string_view text)
